a77c514ddae86acde6236cceef2cf57e766aa4e5
manojpawane <manojpawane@gmail.com>
2017-04-28 17:36:39 +0530

    servlet

---
+++ LoginPage/src/com/controller/LoginPage/Display.java
@@ -0,0 +1,139 @@
+package com.controller.LoginPage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+/**
+ * Servlet implementation class Display
+ */
+@WebServlet("/Display")
+public class Display extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+		
+	{
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String name=request.getParameter("name"); 
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");	
+			 String url="jdbc:mysql://localhost:3306/my1stdatabase";
+			Connection connection=(Connection)DriverManager.getConnection(url,"root","root123");
+			 PreparedStatement ps=connection.prepareStatement("select * from registration where name=?");
+			 ps.setString(1, name);
+		     
+
+           
+                   
+             ResultSet rs=ps.executeQuery();    
+             System.out.println("executed");
+             
+
+            
+
+             java.sql.ResultSetMetaData rsmd=rs.getMetaData();
+
+             while(rs.next())
+
+                {
+            	 
+            	 out.print("<body bgcolor=orange><table width=25% align=center bgcolor=yellow border=1 ></body>");
+            	  out.print("<center><h1>Result:</h1></center>");
+             out.print("<tr>");
+
+             out.print("<td>"+rsmd.getColumnName(1)+"</td>");
+
+                out.print("<td>"+rs.getString(1)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(2)+"</td>");
+
+                out.print("<td>"+rs.getString(2)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(3)+"</td>");
+
+                out.print("<td>"+rs.getString(3)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(4)+"</td>");
+
+                out.print("<td>"+rs.getString(4)+"</td></tr>");  
+                
+                out.print("<td>"+rsmd.getColumnName(5)+"</td>");
+
+                out.print("<td>"+rs.getString(5)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(6)+"</td>");
+
+                out.print("<td>"+rs.getString(6)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(7)+"</td>");
+
+                out.print("<td>"+rs.getString(7)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(8)+"</td>");
+
+                out.print("<td>"+rs.getString(8)+"</td></tr>");                  
+
+                out.print("<td>"+rsmd.getColumnName(9)+"</td>");
+
+                out.print("<td>"+rs.getString(9)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(10)+"</td>");
+
+                out.print("<td>"+rs.getString(10)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(11)+"</td>");
+
+                out.print("<td>"+rs.getString(11)+"</td></tr>");
+
+                out.print("<tr><td>"+rsmd.getColumnName(12)+"</td>");
+
+                out.print("<td>"+rs.getString(12)+"</td></tr>");                  
+                out.print("</table>");
+
+             }
+
+            
+
+
+
+      }catch (Exception e2)
+
+        {
+
+            e2.printStackTrace();
+
+        }
+
+
+
+      finally{out.close();
+		}
+		
+	}
+
+	
+	
+	
+
+}
--- LoginPage/src/com/controller/LoginPage/HelloWorld.java
+++ LoginPage/src/com/controller/LoginPage/HelloWorld.java
@@ -2,6 +2,10 @@ package com.controller.LoginPage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,25 +19,56 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	
-	  
-	 
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String name=request.getParameter("name");
-		String password=request.getParameter("password");
+		String uname = request.getParameter("username");
+		String pword = request.getParameter("password");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
 
-	      // Actual logic goes here.
-	      PrintWriter out = response.getWriter();
-	      out.println("<h1>" + name +"  "+password +"</h1>");
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/my1stdatabase";
+			Connection connection = (Connection) DriverManager.getConnection(url, "root", "root123");
+
+			// connection.setAutoCommit(false);
+			PreparedStatement ps = connection
+					.prepareStatement("select * from registration where username=? and password=? ");
+			ps.setString(1, uname);
+			ps.setString(2, pword);
+
+			ResultSet rs = ps.executeQuery();
+			java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+
+			boolean flag = false;
+
+			while (rs.next()) {
+
+				if (uname.equals(rs.getString(12)) && pword.equals(rs.getString(13))) {
+					response.sendRedirect("view.html");
+
+					flag = true;
+
+				}
+			}
+			if (!flag) {
+
+				out.print("<center><h1>Please Check your Username or Password</h1></center>");
+			}
+
+		} catch (Exception e2)
+
+		{
+
+			e2.printStackTrace();
+
+		}
 	}
 
-	
-	
-	 
 }
